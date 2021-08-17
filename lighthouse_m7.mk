09191758fe8a3563a3274295ece7e11330c0cd5e
Stealth1226 <rishawniyer06@gmail.com>
2021-08-17 09:34:49 +0530

    m7: Light it up

--- lighthouse_m7.mk
+++ lighthouse_m7.mk
@@ -1,11 +1,11 @@
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-$(call inherit-product, vendor/lineage/build/target/product/product_launched_with_j.mk)
+$(call inherit-product, vendor/lighthouse/config/common_full_phone.mk)
+$(call inherit-product, vendor/lighthouse/build/target/product/product_launched_with_j.mk)
 
 $(call inherit-product, device/htc/m7/full_m7.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := m7
-PRODUCT_NAME := lineage_m7
+PRODUCT_NAME := lighthouse_m7
 
 # Device Fingerprint
 BUILD_FINGERPRINT := htc/m7_google/m7:5.1/LMY47O.H18/666675:user/release-keys
